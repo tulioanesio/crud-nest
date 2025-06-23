632684d39728e4ddc063dfd2d98bea52188e1af7
tulioanesio <tulioluv@gmail.com>
2025-06-23 18:27:13 -0300

    feat: integrate Scalar API Reference for improved API documentation

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import {setupSwagger} from "./swagger"
+import { setupSwagger } from './swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(
     new ValidationPipe({
-      whitelist: true,              
-      forbidNonWhitelisted: true,  
-      transform: true,              
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
     }),
   );
-  setupSwagger(app)
+
+  setupSwagger(app);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- src/swagger.ts
+++ src/swagger.ts
@@ -1,14 +1,23 @@
 import { INestApplication } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { apiReference } from '@scalar/nestjs-api-reference';
 
 export function setupSwagger(app: INestApplication) {
   const config = new DocumentBuilder()
     .setTitle('User API')
-    .setDescription('User CRUD API using Nestjs and Prisma')
+    .setDescription('User CRUD API using NestJS and Prisma')
     .setVersion('1.0')
     .addTag('users')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+
+  app.use(
+    '/docs',
+    apiReference({
+      spec: {
+        content: document,
+      },
+    }),
+  );
 }
