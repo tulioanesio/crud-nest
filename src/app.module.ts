d8149029e57d37c5ad2f6fd3ee720baf5b0dcc62
Tulio <tulioluv@gmail.com>
2025-07-30 17:00:54 -0300

    feat: vercel.json added

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { UserController } from './user/user.controller';
 import { UserService } from './user/user.service';
 import { UserModule } from './user/user.module';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [UserModule],
   controllers: [UserController],
-  providers: [UserService],
+  providers: [UserService, PrismaService],
 })
 export class AppModule {}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,38 +1,32 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
 import { CreateUserDto } from 'src/dtos/create-user';
 import { UpdateUserDto } from 'src/dtos/update-user';
-
-const prisma = new PrismaClient();
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class UserService {
-  async createUser(data: CreateUserDto) {
-    const user = await prisma.user.create({
-      data: {
-        name: data.name,
-        email: data.email,
-        password: data.password
-      },
-    });
+  constructor(private prisma: PrismaService) {}
 
+  async createUser(data: CreateUserDto) {
+    const user = await this.prisma.user.create({
+      data,
+    });
     return user;
   }
   async getAllUsers() {
-    return await prisma.user.findMany();
-    }
+    return await this.prisma.user.findMany();
+  }
 
-    async deleteUser(id: number) {
-  return await prisma.user.delete({
-    where: { id },
-  });
-}
+  async deleteUser(id: number) {
+    return await this.prisma.user.delete({
+      where: { id },
+    });
+  }
 
-    async updateUser(id: number, data: UpdateUserDto) {
-  return await prisma.user.update({
-    where: { id },
-    data,
-  });
-}
-
+  async updateUser(id: number, data: UpdateUserDto) {
+    return await this.prisma.user.update({
+      where: { id },
+      data,
+    });
+  }
 }
